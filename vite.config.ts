5185aa6c8c920e3c465acfd0c9f772e8327733a2
matkrin <matkrin@protonmail.com>
2023-08-16 09:24:49 +0200

    Installabe as PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+    registerType: "autoUpdate",
+    includeAssets: ["favicon.ico", "pwa-192x192.png", "pwa-512x512.png"],
+    manifest: {
+        name: "Webplot",
+        short_name: "Webplot",
+        description: "Plot data",
+        theme_color: "#ffffff",
+        //start_url: "/",
+        //background_color: "ffffff",
+        //display: "standalone",
+        //orientation: "portrait",
+        icons: [
+            {
+                src: "pwa-512x512.png",
+                type: "image/png",
+                sizes: "512x512",
+            },
+            {
+                src: "pwa-192x192.png",
+                type: "image/png",
+                sizes: "192x192",
+            },
+            {
+                src: "pwa-512x512.png",
+                type: "image/png",
+                sizes: "512x512",
+                purpose: "maskable",
+            },
+        ],
+    },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), VitePWA(manifestForPlugin)],
     base: "/webplot/",
 });
