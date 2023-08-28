8baa160f47873a549278284a8f7d90ab3a47aad1
matkrin <matkrin@protonmail.com>
2023-08-28 23:06:33 +0200

    Moved NormalizationSlider out of AesPlot

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,22 @@ import "./App.css";
 import { useStore } from "./store";
 import FullWindowDropzone from "./components/FullWindowDropzone";
 import AesPlot from "./components/AesPlot";
+import NormalizationSlider from "./components/NormalizationSlider";
+import Smoothing from "./components/Smoothing";
 
 function App() {
     const aesFiles = useStore((state) => state.aesFiles);
     return (
         <FullWindowDropzone>
             <div className="App">
-                {aesFiles.length > 0 && <AesPlot />}
+                {aesFiles.length > 0 &&
+                    (
+                        <>
+                            <AesPlot />
+                            <NormalizationSlider />
+                            <Smoothing />
+                        </>
+                    )}
             </div>
         </FullWindowDropzone>
     );
