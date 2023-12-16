8b6111ffdd095340404e90781d80e9b86f564617
matkrin <matkrin@protonmail.com>
2023-12-16 08:09:05 +0100

    Logfile parameter extraction

--- src/mulfile/index.ts
+++ src/mulfile/index.ts
@@ -1,3 +1,4 @@
+import { LogFile } from "../logfile";
 import { MulImage, parseMul } from "./parse";
 
 export class MulFile {
@@ -18,5 +19,27 @@ export class MulFile {
         this.imgs.forEach((img) =>
             img.imgID = `${filename.split(".")[0]}_${img.imgNum}`
         );
+    }
+
+    extractLogFile(logfile: LogFile): void {
+        this.imgs.forEach((img) => {
+            const datetimes = logfile.datetimes;
+            const deltas = datetimes.map((dt) =>
+                Math.abs(img.datetime.getTime() - dt.getTime())
+            ).filter((x) => x);
+            const minDelta = Math.min(...deltas);
+
+            // logging in Prodigy should be at least every 2 seconds
+            if (minDelta < 2000) {
+                const minIdx = deltas.findIndex((x) => x === minDelta);
+                img.logFileData = {
+                    datetime: logfile.datetimes[minIdx],
+                    pStmChamber: logfile.pStmChamber[minIdx],
+                    pPrepChamber: logfile.pPrepChamber[minIdx],
+                    pStmInlet: logfile.pStmInlet[minIdx],
+                    tempSample: logfile.tempSample[minIdx],
+                };
+            }
+        });
     }
 }
