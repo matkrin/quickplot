3376f0a73d5520b8549a05f5f6f34b51a0345cc6
matkrin <matkrin@protonmail.com>
2023-12-17 17:41:22 +0100

    store update for lightbox

--- src/stores/mul_slice.ts
+++ src/stores/mul_slice.ts
@@ -1,14 +1,22 @@
 import { StateCreator } from "zustand";
 import { MulFile } from "../mulfile";
+import { MulImage } from "../mulfile/parse";
 
 export type MulSlice = {
     mulFiles: Array<MulFile>;
+    selectedImage: MulImage | null;
+    isLightboxOpen: boolean;
+    setSelectedImage: (mulImage: MulImage) => void;
     addMulFile: (newFile: MulFile) => void;
+    openLightbox: (mulImage: MulImage) => void;
+    closeLightbox: () => void;
 };
 
 export const createMulSlice: StateCreator<MulSlice> = (set) => {
     return {
         mulFiles: [],
+        selectedImage: null,
+        isLightboxOpen: false,
 
         addMulFile: (newFile) => {
             set((state) => {
@@ -17,5 +25,23 @@ export const createMulSlice: StateCreator<MulSlice> = (set) => {
                 }) };
             });
         },
+
+        setSelectedImage: (mulimage) => {
+            set(() => {
+                return { selectedImage: mulimage };
+            })
+        },
+
+        openLightbox: (mulimage) => {
+            set(() => {
+                return { selectedImage: mulimage, isLightboxOpen: true }; 
+            })
+        },
+
+        closeLightbox: () => {
+                set(() => {
+                    return { selectedImage: null, isLightboxOpen: false }
+                })
+            }
     };
 };
